cfd73835e22f18702336f1d3446cbe059c1d4cb1
Indraja45 <indraja.pakalapati.work@gmail.com>
2025-07-22 09:40:42 +0200

    changed endpoints and logic

--- src/main/java/com/example/fraud/service/AgentService.java
+++ src/main/java/com/example/fraud/service/AgentService.java
@@ -1,7 +1,11 @@
 package com.example.fraud.service;
 
+import com.example.fraud.model.CustomerNotification;
+import com.example.fraud.model.EmailContent;
 import com.example.fraud.model.Transaction;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -28,10 +33,12 @@ public class AgentService {
     @Autowired
     RestTemplate restTemplate;
 
-    private static final String MCP_CLIENT_URL = "http://localhost:8090/client/findFraudTransactions"; // Update if needed
+    private static final String MCP_CLIENT_URL = "http://localhost:8090/api/client/findFraudTransactions"; // Update if needed
+
+    private static final String MCP_EMAIL_CLIENT_URL = "http://localhost:8090/api/client/sendEmailNotification"; // Update if needed
 
 
-    public ResponseEntity<String> fraudClientServiceTransactions(@RequestBody String prompt) {
+    public ResponseEntity<String> fraudClientServiceTransactions(String prompt) {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
@@ -45,4 +52,53 @@ public class AgentService {
                     .body("Error occurred: " + e.getMessage());
         }
     }
+
+    public ResponseEntity<String> emailClientNotification(EmailContent emailContent) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            ObjectMapper objectMapper  = new ObjectMapper();
+            String jsonBody = objectMapper.writeValueAsString(emailContent);
+            HttpEntity request = new HttpEntity<>(jsonBody,headers);
+
+            ResponseEntity<String> response = restTemplate.postForEntity(MCP_EMAIL_CLIENT_URL, request, String.class);
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error occurred: " + e.getMessage());
+        }
+    }
+
+    public ResponseEntity<String> sendNotification(String responseEntity) throws JsonProcessingException {
+//        String jsonResponse = String.valueOf(responseEntity);
+
+
+        int start = responseEntity.indexOf('[');
+        int end = responseEntity.lastIndexOf(']') + 1;
+        String json = responseEntity.substring(start, end);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(json);
+        List<CustomerNotification> customerObject = new ArrayList<>();
+
+        for (JsonNode node : root) {
+            CustomerNotification summary = new CustomerNotification();
+            summary.setCustomerId(node.get("customerId").asText());
+            summary.setFraudReason(node.get("reason").asText());
+
+            EmailContent emailContent = new EmailContent();
+            emailContent.setRecipientId("hemanth.kakarlapudi@capgemini.com");
+            emailContent.setSubject("Test Email from Postman");
+            emailContent.setBody("This is a test email sent using Azure Communication Services from Spring Boot");
+            summary.setEmailContent(emailContent);
+            System.out.println(summary.getCustomerId());
+            System.out.println(summary.getFraudReason());
+            customerObject.add(summary);
+        }
+        System.out.println("Size-->"+customerObject.size());
+        return ResponseEntity.ok(customerObject.toString());
+    }
+
 }
